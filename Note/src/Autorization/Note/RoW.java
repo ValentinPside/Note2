086606b2e14e5da3af109b4821a44c224ed9e63b
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2022-02-26 19:55:04 +0300

    pre
    1я лабораторная работа, которую нужно было модифицировать

--- Note/src/Autorization/Note/RoW.java
+++ Note/src/Autorization/Note/RoW.java
@@ -6,11 +6,8 @@ package Autorization.Note;
 
 import Autorization.User;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import javafx.scene.control.TextArea;
 
 
 /**
@@ -30,25 +27,5 @@ public class RoW {
             e.getMessage();
         }
         
-    }
-    
-    public static String readFile(User user, TextArea textArea) {
-        String text = "";
-            try{
-                File file = new File(user.getId() + ".txt");
-                FileReader fReader = new FileReader(file);
-                char[] temp = new char[1024];
-                int i;
-                while ((i = fReader.read(temp)) > 0){
-                    String line = new String(temp, 0, i);
-                    textArea.appendText(line);
-                return text;
-            } 
-            }catch (FileNotFoundException ex) {
-                ex.getMessage();
-            } catch (IOException ex) {
-                ex.getMessage();
-            }
-        return text;
     }
 }
